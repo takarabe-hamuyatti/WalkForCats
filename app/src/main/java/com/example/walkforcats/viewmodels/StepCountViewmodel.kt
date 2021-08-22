e746fdf84178168a2e279bb5d44a043c48e95cf7
hamuyatti <syosatci@gmail.com>
2021-08-22 17:40:10 +0900

    sensorの機能を　viewmodelに移行

--- app/src/main/java/com/example/walkforcats/viewmodels/StepCountViewmodel.kt
+++ app/src/main/java/com/example/walkforcats/viewmodels/StepCountViewmodel.kt
@@ -1,10 +1,18 @@
 package com.example.walkforcats.viewmodels
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.walkforcats.listener.StepListener
+import com.example.walkforcats.utils.StepDetector
 
-class StepCountViewmodel:ViewModel(){
+class StepCountViewmodel():ViewModel(), SensorEventListener , StepListener{
+    var sensorManager: SensorManager? = null
+    var simpleStepDetector: StepDetector? = null
 
 
     private var _percent = MutableLiveData<Float>()
@@ -24,6 +32,39 @@ class StepCountViewmodel:ViewModel(){
     fun getParcent(){
         var tmp = _count.value?.toFloat()?.times(100)
         _percent.value = tmp?.div(1000)
+    }
+
+     fun getSensorManager(sensor:SensorManager){
+             sensorManager = sensor
+             simpleStepDetector = StepDetector()
+             simpleStepDetector!!.registerListener(this)
+             if (sensorManager == null) {
+                 /*Toast.makeText(context, "端末にセンサーが用意されていません。", Toast.LENGTH_SHORT).show()
+
+                  */
+             } else {
+                 sensorManager!!.registerListener(
+                     this,
+                     sensorManager!!.getDefaultSensor(android.hardware.Sensor.TYPE_ACCELEROMETER),
+                     SensorManager.SENSOR_DELAY_FASTEST
+                 )
+             }
+     }
+
+
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector!!.updateAccelerometer(event.timestamp, event.values[0], event.values[1], event.values[2])
+        }
+    }
+
+    override fun onAccuracyChanged(p0: android.hardware.Sensor?, p1: Int) {
+    }
+
+    override fun step(timeNs: Long) {
+        plusCount()
+        getParcent()
     }
 
 
